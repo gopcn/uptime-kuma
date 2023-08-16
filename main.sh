b52934a73d485205cb34fb9534c33be23589fb9d
神经蛙 <121377609+gopcn@users.noreply.github.com>
2023-08-16 11:43:56 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/louislam/uptime-kuma.git
+cd uptime-kuma
+npm run setup
+
+# Option 1. Try it
+node server/server.js
+
+# (Recommended)
+# Option 2. Run in background using PM2
+# Install PM2 if you don't have: npm install pm2 -g
+pm2 start server/server.js --name uptime-kuma
